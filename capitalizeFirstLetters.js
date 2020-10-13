66b7b2923a180a7b744bda22f1bf080e6bb7ed1a
Alexandre POIRRIER <alex.poirrier@gmail.com>
2020-10-13 18:23:35 +0200

    ADD the function

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -1,5 +1,10 @@
 const assert = require('assert');
 
+function capitalizeFirstLetters(input) {
+    let t = input.split(' ');
+    return t.map((e) => e.charAt(0).toUpperCase() + e.slice(1)).join(' ')
+}
+
 assert.strictEqual(typeof capitalizeFirstLetters, 'function');
 assert.strictEqual(capitalizeFirstLetters.length, 1);
 assert.strictEqual(capitalizeFirstLetters('javaScript'), 'JavaScript');
